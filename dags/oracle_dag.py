a6198512f05e0e6fbf4e034ecf738beba48451ff
Benji Lampel <benjamin@astronomer.io>
2022-04-07 10:37:02 -0400

    Reformatted dag file to conform to Certified DAG style conventions
    - Removed unused imports
    - Now using Pendulum for timezone-aware datetimes
    - Removed default_args dict
    - Added Oracle conn variable, which is the only default arg
    - Updated task name to match task_id
    - Removed oracle_conn_id param as it is a default arg
    - Reformatted file using black

--- dags/oracle_dag.py
+++ dags/oracle_dag.py
@@ -1,54 +1,36 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-from airflow.providers.oracle.operators.oracle import OracleOperator,OracleStoredProcedureOperator
-from airflow.version import version
-from datetime import datetime, timedelta
+from airflow.providers.oracle.operators.oracle import (
+    OracleOperator,
+    OracleStoredProcedureOperator,
+)
+from pendulum import datetime
 
 
+my_oracle_conn = "my_oracle_conn"
+with DAG(
+    "oracle_dag",
+    start_date=datetime(2019, 1, 1),
+    max_active_runs=3,
+    schedule_interval=None,
+    default_args=my_oracle_conn,
+    catchup=False,
+    template_searchpath="/usr/local/airflow/include/sql",
+) as dag:
 
-# Default settings applied to all tasks
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
-
-# Using a DAG context manager, you don't have to specify the dag property of each task
-with DAG('oracle_dag',
-         start_date=datetime(2019, 1, 1),
-         max_active_runs=3,
-         schedule_interval=None,
-         default_args = default_args,
-         catchup = False,
-        template_searchpath='/usr/local/airflow/include/sql'
-
-         ) as dag:
-
-    t0 = OracleOperator(
-        task_id='ddl',
-        sql='ddl.sql',
-        oracle_conn_id='my_oracle_conn'
-
+    ddl = OracleOperator(
+        task_id="ddl",
+        sql="ddl.sql",
     )
 
-    t1 = OracleOperator(
-        task_id='insert',
-        sql='insert.sql',
-        oracle_conn_id='my_oracle_conn'
-
+    insert = OracleOperator(
+        task_id="insert",
+        sql="insert.sql",
     )
 
-    t2 = OracleStoredProcedureOperator(task_id='call_procedure',
-                                       oracle_conn_id='my_oracle_conn',
-                                       procedure='remove_prod',
-                                       parameters={'product_id':1})
+    call_procedure = OracleStoredProcedureOperator(
+        task_id="call_procedure",
+        procedure="remove_prod",
+        parameters={"product_id": 1},
+    )
 
-
-
-
-    t0 >> t1  >> t2
+    ddl >> insert >> call_procedure
